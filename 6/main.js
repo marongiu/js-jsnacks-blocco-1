4362410140f55b8d069b4842f0bc284a059bb601
Andrea <andreamarongiu05@gmail.com>
2021-03-03 16:27:13 +0100

    continuo esercizio 6

--- 6/main.js
+++ 6/main.js
@@ -4,11 +4,6 @@ var numero = parseInt(prompt("Dammi un numero"));
 
 // Cubo del numero
 
-var numeroCubo = Math.pow(numero, 3);
-
-// Stampo il numero al cubo
-console.log(numeroCubo);
-
-var numeroAlternativa = (numero * numero * numero)
-
-console.log(numeroAlternativa);
+for (var i = 0; i < numero; i++) {
+  console.log(i);
+}
